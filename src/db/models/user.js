10f4437f759ecbd78271fbdd60492713d7f6c286
Alen Thankachan <alenthankz@gmail.com>
2020-06-26 07:42:44 -0700

    mongodb added

---
+++ src/db/models/user.js
@@ -0,0 +1,44 @@
+const mongoose =require('mongoose');
+const { default: validator } = require('validator');
+
+
+const User =mongoose.model('user',{
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error('invalid email');
+            }
+        }
+    },
+    age:{
+        type:Number,
+        default:0,
+        validate(value){
+            if(value<0){
+                throw new Error('age must be valid number');
+            }
+        }
+    }
+})
+// //when saved to mongodb user small u not captial U 
+//  const me =new User({
+//      name:'alen thankachan     ',
+//      email:'123@gmail.com',
+//      age:27
+//  })
+
+//  me.save().then(()=>{
+//     console.log(me)
+//  }).catch((error)=>{
+//      console.log(error)
+//  })
+
+
+ module.exports =User
